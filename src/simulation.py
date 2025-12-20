7a03509d19a799a9c2d2a8769312394e1a4f4461
Вячеслав Смирнов <smslava7@gmail.com>
2025-12-20 18:14:34 +0300

    logging

--- src/simulation.py
+++ src/simulation.py
@@ -6,6 +6,7 @@ from src.books.printed_book import PrintedBook
 from src.constants import AUTHORS, EBOOK_FORMATS, GENRES, NARRATORS, TITLES, YEARS
 from src.exceptions import BookValidationError, LibraryError, LibraryIndexError
 from src.library import Library
+from src.setup_logger import logger
 
 
 def generate_random_book() -> BaseBook:
@@ -56,7 +57,9 @@ def run_simulation(steps: int = 20, seed: int | None = None) -> None:
 
     lib = Library()
 
-    print(f'Начало симуляции (steps: {steps}, seed: {seed})')
+    text = f'Начало симуляции (steps: {steps}, seed: {seed})'
+    print(text)
+    logger.info(text)
     for i in range(random.randint(3, 11)):
         try:
             lib.add_book(generate_random_book())
@@ -71,39 +74,53 @@ def run_simulation(steps: int = 20, seed: int | None = None) -> None:
     for step in range(1, steps + 1):
         action = random.choice(actions)
         print(f'{step:02d}. {action.upper():<16} | ', end='')
+        text = f'{action.upper()} | '
         try:
             match action:
                 case 'add':
                     book = generate_random_book()
                     lib.add_book(book)
+                    text += f'OK. Добавлена {book}'
                     print(f'OK. Добавлена {book}')
+                    logger.info(text)
 
                 case 'remove':
                     if len(lib) > 0:
                         index = random.randint(0, len(lib) - 1)
                         book = lib.books[index]
                         lib.remove_book(book)
+                        text += f'OK. Удалена {book}'
                         print(f'OK. Удалена {book}')
+                        logger.info(text)
                     else:
+                        text += 'FAILED. Удаление не удалось. Библиотека пуста'
                         print('FAILED. Удаление не удалось. Библиотека пуста')
+                        logger.warning(text)
 
                 case 'search_by_genre':
                     genre = random.choice(GENRES)
                     result = lib.find_by_genre(genre)
+                    text += f'OK. Поиск по жанру "{genre}": найдено {len(result)}'
                     print(f'OK. Поиск по жанру "{genre}": найдено {len(result)}')
+                    logger.info(text)
 
                 case 'search_by_author':
                     author = random.choice(AUTHORS)
                     result = lib.find_by_author(author)
+                    text += f'OK. Поиск по автору "{author}": найдено {len(result)}'
                     print(f'OK. Поиск по автору "{author}": найдено {len(result)}')
+                    logger.info(text)
 
                 case 'search_by_year':
                     year = random.choice(YEARS)
                     result = lib.find_by_year(year)
+                    text += f'OK. Поиск по {year} году: найдено {len(result)}'
                     print(f'OK. Поиск по {year} году: найдено {len(result)}')
+                    logger.info(text)
 
                 case 'fail_isbn':
                     fake_isbn = '666-FAIL'
+                    text += f'Проверка вызова ISBN {fake_isbn}: '
                     print(f'Проверка вызова ISBN {fake_isbn}: ', end='')
                     _ = lib.isbn_index[fake_isbn]
 
@@ -113,27 +130,43 @@ def run_simulation(steps: int = 20, seed: int | None = None) -> None:
                         lib.remove_book(target)
                         target.year = random.choice(YEARS)
                         lib.add_book(target)
+                        text += f'OK. Индекс {target} обновлен'
                         print(f'OK. Индекс {target} обновлен')
+                        logger.info(text)
                     else:
+                        text += 'SKIPPED. Библиотека пуста'
                         print('SKIPPED. Библиотека пуста')
+                        logger.warning(text)
 
                 case 'add_duplicate':
                     if len(lib) > 0:
                         existing_book = lib.books[0]
+                        text += f'Попытка добавить дубликат (ISBN: {existing_book.isbn}) '
                         print(f'Попытка добавить дубликат (ISBN: {existing_book.isbn}) ', end='')
                         lib.add_book(existing_book)
                     else:
+                        text += 'Библиотека пуста. Тест пропущен'
                         print('Библиотека пуста. Тест пропущен')
+                        logger.warning(text)
         except LibraryIndexError:
-            print('Ошибка: Книга с таким ключом отсутствует в системе')
+            text += 'Ошибка: Книга с таким ключом отсутствует в системе'
+            print(text)
+            logger.error(text)
         except BookValidationError as e:
-            print(f'Ошибка валидации: {e}')
+            text += f'Ошибка валидации: {e}'
+            print(text)
+            logger.error(text)
         except LibraryError as e:
-            print(f'Системная ошибка: {e}')
+            text += f'Системная ошибка: {e}'
+            print(text)
+            logger.error(text)
         except Exception as e:
-            print(f'Непредвиденная ошибка: {e}')
-
-    print(f'Конец симуляции. Всего книг: {len(lib)}')
+            text += f'Непредвиденная ошибка: {e}'
+            print(text)
+            logger.error(text)
+    text = f'Конец симуляции. Всего книг: {len(lib)}'
+    print(text)
+    logger.info(text)
     print('Библиотека:')
     if len(lib) > 1:
         print_slice = [book for book in lib.books[0:2]]
